e190e3eff790e2afe4d3584e2f703854d52bb0f5
Mirdinic <mirdin.sadykov@mail.ru>
2021-08-14 12:21:30 +0600

    Initial Commit

---
+++ weatherapp/weatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappApp.swift
+//  weatherapp
+//
+//  Created by Mirdin Sadykov on 8/14/21.
+//
+
+import SwiftUI
+
+@main
+struct weatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
